adb274830a7ea8eb78315c379d51919c8c4471db
SEYEON_PARK <fourleaf123@naver.com>
2021-02-25 00:42:50 +0900

    C언어 <ctype.h>의 isalnum(영문자이거나 숫자인지 확인해준다.) 함수 연습!(2)

---
+++ C_isalnum_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	char a;
+
+	printf("아무거나 입력하세요. : ");
+	scanf_s("%c", &a, sizeof(a));
+
+	if (isalnum(a) != 0)
+	{
+		printf("입력하신 것은 영문자이거나 숫자입니다.\n");
+	}
+	else
+	{
+		printf("입력하신 것은 영문자도 아니고 숫자도 아닙니다.\n");
+	}
+
+	return 0;
+}
